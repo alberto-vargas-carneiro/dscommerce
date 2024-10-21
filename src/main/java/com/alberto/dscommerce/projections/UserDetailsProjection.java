f90b839c17a5e00461ed78b84f9c21fb8a33b65f
Alberto <carneiroalberto63@gmail.com>
2024-10-21 15:38:54 -0300

    11

---
+++ src/main/java/com/alberto/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.alberto.dscommerce.projections;
+
+public interface UserDetailsProjection {
+    
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+}
